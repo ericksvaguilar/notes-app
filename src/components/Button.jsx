6b197f84383c592ee51ef28d45499cefd80771a5
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-01 15:11:32 -0300

    fix: remove submit from button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,15 +3,5 @@ import { NoteContext } from '../contexts/NoteContext'
 import styles from '../styles/components/Button.module.css'
 
 export function Button() {
-  const { addNote, inputNote } = useContext(NoteContext)
-
-  function handleSubmit() {
-    addNote(inputNote)
-  }
-
-  return (
-    <button className={styles.button} onSubmit={handleSubmit}>
-      New Note
-    </button>
-  )
+  return <input type='submit' value='new note' className={styles.button} />
 }
