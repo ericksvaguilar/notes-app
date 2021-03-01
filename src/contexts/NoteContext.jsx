92f64abbda0c398d1befe8958bf939dee3a56605
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-01 14:48:01 -0300

    feat(notecontext): add input note state

--- src/contexts/NoteContext.jsx
+++ src/contexts/NoteContext.jsx
@@ -4,13 +4,14 @@ export const NoteContext = createContext()
 
 export function NoteProvider({ children }) {
   const [notes, setNotes] = useState(['This is a note', 'This is another note'])
+  const [inputNote, setInputNote] = useState('')
 
   function addNote(note) {
     setNotes(prev => [...prev, note])
   }
 
   return (
-    <NoteContext.Provider value={{ notes, addNote }}>
+    <NoteContext.Provider value={{ notes, addNote, inputNote, setInputNote }}>
       {children}
     </NoteContext.Provider>
   )
