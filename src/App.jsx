874b042035698998296d8e9819889f7224f86879
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-01 14:43:31 -0300

    feat(app): add input component to app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styles from './styles/App.module.css'
 
 import { Button } from './components/Button'
 import { Note } from './components/Note'
+import { Input } from './components/Input'
 
 import { NoteProvider } from './contexts/NoteContext'
 
@@ -11,6 +12,7 @@ function App() {
     <NoteProvider>
       <div className={styles.app}>
         <h1>React Note</h1>
+        <Input />
         <Button />
         <Note />
       </div>
