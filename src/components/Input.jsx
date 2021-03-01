193f0dfa497cc454e7d77dfda1dab2f0a197d94d
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-01 15:12:02 -0300

    fix: add submit even handler to input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,14 +4,19 @@ import { NoteContext } from '../contexts/NoteContext'
 import styles from '../styles/components/Input.module.css'
 
 export function Input(props) {
-  const { inputNote, setInputNote } = useContext(NoteContext)
+  const { addNote, inputNote, setInputNote } = useContext(NoteContext)
 
   function handleChange({ target }) {
     setInputNote(target.value)
   }
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    addNote(inputNote)
+  }
+
   return (
-    <form className={styles.inputContainer}>
+    <form className={styles.inputContainer} onSubmit={handleSubmit}>
       <input type='text' value={inputNote} onChange={handleChange} required />
       {props.children}
     </form>
