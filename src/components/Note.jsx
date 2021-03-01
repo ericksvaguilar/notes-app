9aa499e6e3e547fc6eca50600d22aecb03f12b96
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-01 15:57:31 -0300

    feat: add remove note feature

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,12 +2,23 @@ import React, { useContext } from 'react'
 import { NoteContext } from '../contexts/NoteContext'
 
 export function Note() {
-  const { notes } = useContext(NoteContext)
+  const { notes, removeNote } = useContext(NoteContext)
+
+  function handleRemoveNote(value) {
+    removeNote(value)
+  }
 
   return (
     <div>
       {notes.map(note => (
-        <li key={String(note)}>{note}</li>
+        <>
+          <li key={String(note)}>
+            <input type='text' readOnly='true' value={note} />
+            <button type='button' onClick={() => removeNote(note)}>
+              Remove
+            </button>
+          </li>
+        </>
       ))}
     </div>
   )
