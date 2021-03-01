ce124d98ebf6fc33aa34ae48f520943ebff298a7
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-01 14:49:56 -0300

    feat(input): set new input note useContext

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { NoteContext } from '../contexts/NoteContext'
 
 export function Input() {
-  function handleChange({ target }) {}
+  const { inputNote, setInputNote } = useContext(NoteContext)
+
+  function handleChange({ target }) {
+    setInputNote(target.value)
+  }
 
   return (
     <div>
-      <input type='text' onChange={handleChange} />
+      <input type='text' value={inputNote} onChange={handleChange} />
     </div>
   )
 }
