95be44a9398d13171d2c1cc25b855d3df7b0412f
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-03 07:43:22 -0300

    feat: clear input on submit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -14,6 +14,7 @@ export function Form(props) {
   function handleSubmit(e) {
     e.preventDefault()
     addNote({ id: nanoid(), text: inputNote, isEditing: false })
+    setInputNote('')
   }
 
   return (
